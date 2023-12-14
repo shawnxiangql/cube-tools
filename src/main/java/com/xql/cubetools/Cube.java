64615ba190a4bdf833698a18fe42b45b0e418086
shawn.xiang <shawn.xiang@farfetch.com>
2023-12-14 18:03:06 +0800

    update

--- src/main/java/com/xql/cubetools/Cube.java
+++ src/main/java/com/xql/cubetools/Cube.java
@@ -1,9 +1,8 @@
 package com.xql.cubetools;
 
 import java.util.*;
+import java.util.concurrent.atomic.AtomicInteger;
 import java.util.function.Consumer;
-
-import static com.xql.cubetools.Slice.*;
 
 /**
  * <pre>
@@ -31,7 +30,14 @@ import static com.xql.cubetools.Slice.*;
  * </pre>
  */
 public class Cube {
-    private final Slice[] slices = Slice.values();
+    private final int[] slices = {
+            U1, U2, U3, U4, U5, U6, U7, U8, U9,
+            R1, R2, R3, R4, R5, R6, R7, R8, R9,
+            F1, F2, F3, F4, F5, F6, F7, F8, F9,
+            D1, D2, D3, D4, D5, D6, D7, D8, D9,
+            L1, L2, L3, L4, L5, L6, L7, L8, L9,
+            B1, B2, B3, B4, B5, B6, B7, B8, B9,
+    };
 
     private static final EnumMap<Slice, Integer> si;
 
@@ -72,7 +78,7 @@ public class Cube {
     /**
      * a -> b -> c -> d -> a
      */
-    public Cube loopSwap(Slice a, Slice b, Slice c, Slice d) {
+    public Cube loopSwap(int a, int b, int c, int d) {
         int indexA = si.get(a);
         int indexB = si.get(b);
         int indexC = si.get(c);
@@ -85,17 +91,17 @@ public class Cube {
         return this;
     }
 
-    private Slice edgeBuffer = U6;
-    private Slice cornerBuffer = U9;
+    private int edgeBuffer = U6;
+    private int cornerBuffer = U9;
 
-    private Slice[] edgeFind = {U8, U4, U2, D2, D4, D8, D6, F4, F6, B6, B4};
+    private int[] edgeFind = {U8, U4, U2, D2, D4, D8, D6, F4, F6, B6, B4};
 
     public List<List<String>> getCodes() {
         List<String> edgeCodes = new ArrayList<>();
         List<Slice> edgeFlip = new ArrayList<>();
 
         Set<Block> blocks = new HashSet<>();
-        Slice current = edgeBuffer;
+        int current = edgeBuffer;
         Block currentBlock = current.getBlock();
         blocks.add(currentBlock);
 
@@ -141,52 +147,136 @@ public class Cube {
     }
 }
 
-enum Slice {
-    U1, U2, U3, U4, U5, U6, U7, U8, U9,
-    R1, R2, R3, R4, R5, R6, R7, R8, R9,
-    F1, F2, F3, F4, F5, F6, F7, F8, F9,
-    D1, D2, D3, D4, D5, D6, D7, D8, D9,
-    L1, L2, L3, L4, L5, L6, L7, L8, L9,
-    B1, B2, B3, B4, B5, B6, B7, B8, B9,
-    ;
 
-    Block getBlock() {
-        for (Block block : Block.values()) {
-            if (block.slices.contains(this)) return block;
+class Slice {
+    private static final AtomicInteger index = new AtomicInteger(0);
+    private final int id;
+
+    public Slice() {
+        this.id = index.getAndIncrement();
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Slice slice = (Slice) o;
+        return id == slice.id;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+
+    Slice U1 = new Slice(), U2 = new Slice(), U3 = new Slice(), U4 = new Slice(), U5 = new Slice(), U6 = new Slice(), U7 = new Slice(), U8 = new Slice(), U9 = new Slice(),
+            R1 = new Slice(), R2 = new Slice(), R3 = new Slice(), R4 = new Slice(), R5 = new Slice(), R6 = new Slice(), R7 = new Slice(), R8 = new Slice(), R9 = new Slice(),
+            F1 = new Slice(), F2 = new Slice(), F3 = new Slice(), F4 = new Slice(), F5 = new Slice(), F6 = new Slice(), F7 = new Slice(), F8 = new Slice(), F9 = new Slice(),
+            D1 = new Slice(), D2 = new Slice(), D3 = new Slice(), D4 = new Slice(), D5 = new Slice(), D6 = new Slice(), D7 = new Slice(), D8 = new Slice(), D9 = new Slice(),
+            L1 = new Slice(), L2 = new Slice(), L3 = new Slice(), L4 = new Slice(), L5 = new Slice(), L6 = new Slice(), L7 = new Slice(), L8 = new Slice(), L9 = new Slice(),
+            B1 = new Slice(), B2 = new Slice(), B3 = new Slice(), B4 = new Slice(), B5 = new Slice(), B6 = new Slice(), B7 = new Slice(), B8 = new Slice(), B9 = new Slice();
+
+}
+
+class Block {
+    private final Slice[] slices;
+
+    private Block(Slice... slices) {
+        this.slices = slices;
+    }
+
+    //    UFL = {U7, F1, L3},
+//    ULB = {U1, L1, B3},
+//    UBR = {U3, B1, R3},
+//    URF = {U9, F3, R1},
+//    DLF = {D1, L9, F7},
+//    DBL = {D7, B9, L7},
+//    DRB = {D9, R9, B7},
+//    DFR = {D3, F9, R7},
+//    UF = {U8, F2},
+//    UL = {U4, L2},
+//    UB = {U2, B2},
+//    UR = {U6, R2},
+//    DF = {D2, F8},
+//    DL = {D4, L8},
+//    DB = {D8, B8},
+//    DR = {D6, R8},
+//    FR = {F6, R4},
+//    FL = {F4, L6},
+//    BL = {B6, L4},
+//    BR = {B4, R6};
+    public static final Block
+            UFL = new Block(Slice.U7, Slice.F1, Slice.L3),
+            ULB = new Block(Slice.U1, Slice.L1, Slice.B3),
+            UBR = new Block(Slice.U3, Slice.B1, Slice.R3),
+            URF = new Block(Slice.U9, Slice.F3, Slice.R1),
+            DLF = new Block(Slice.D1, Slice.L9, Slice.F7),
+            DBL = new Block(Slice.D7, Slice.B9, Slice.L7),
+            DRB = new Block(Slice.D9, Slice.R9, Slice.B7),
+            DFR = new Block(Slice.D3, Slice.F9, Slice.R7),
+            UF = new Block(Slice.U8, Slice.F2),
+            UL = new Block(Slice.U4, Slice.L2),
+            UB = new Block(Slice.U2, Slice.B2),
+            UR = new Block(Slice.U6, Slice.R2),
+            DF = new Block(Slice.D2, Slice.F8),
+            DL = new Block(Slice.D4, Slice.L8),
+            DB = new Block(Slice.D8, Slice.B8),
+            DR = new Block(Slice.D6, Slice.R8),
+            FR = new Block(Slice.F6, Slice.R4),
+            FL = new Block(Slice.F4, Slice.L6),
+            BL = new Block(Slice.B6, Slice.L4),
+            BR = new Block(Slice.B4, Slice.R6);
+
+}
+
+interface Const {
+    // slices
+
+    // blocks
+//    int UFL = 1, ULB = 2, UBR = 3, URF = 4, DLF = 5, DBL = 6, DRB = 7, DFR = 8,
+//            UF = 1, UL = 2, UB = 3, UR = 4, DF = 5, DL = 6, DB = 7, DR = 8, FR = 9, FL = 10, BL = 11, BR = 12;
+
+    Map<Integer, Integer> blockMap = new HashMap<>() {
+        {
+
+            put(U7, UFL);
+            put(F1, UFL);
+            put(L3, UFL);
+            put(U1, ULB);
+            put(L1, ULB);
+            put(B3, ULB);
+            put(U3, UBR);
+            put(B1, UBR);
+            put(R3, UBR);
+            put(U9, URF);
+            put(F3, URF);
+            put(R1, URF);
+            put(D1, DLF);
+            put(L9, DLF);
+            put(F7, DLF);
+            put(D7, DBL);
+            put(B9, DBL);
+            put(L7, DBL);
+            put(D9, DRB);
+            put(R9, DRB);
+            put(B7, DRB);
+            put(D3, DFR);
+            put(F9, DFR);
+            put(R7, DFR);
+            put(U8, UF);
+            put(F2, UF);
+            put(U4, UL);
+            put(L2, UL);
+            put(U2, UB);
+            put(B2, UB);
+            put(U6, UR);
+            put(R2, UR);
+            put(D2, DF);
+            put(F8, DF);
         }
-        return null;
-    }
+    };
 }
 
-enum Block {
-    UFL(U7, F1, L3),
-    ULB(U1, L1, B3),
-    UBR(U3, B1, R3),
-    URF(U9, F3, R1),
-    DLF(D1, L9, F7),
-    DBL(D7, B9, L7),
-    DRB(D9, R9, B7),
-    DFR(D3, F9, R7),
-    UF(U8, F2),
-    UL(U4, L2),
-    UB(U2, B2),
-    UR(U6, R2),
-    DF(D2, F8),
-    DL(D4, L8),
-    DB(D8, B8),
-    DR(D6, R8),
-    FR(F6, R4),
-    FL(F4, L6),
-    BL(B6, L4),
-    BR(B4, R6),
-    ;
-
-    public final List<Slice> slices;
-
-    Block(Slice... slices) {
-        this.slices = Arrays.asList(slices);
-    }
-}
 
 enum Ops {
     OP_E("E", cube -> cube
